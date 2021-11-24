2713f98ae065159d9dc8301f2b7b3e4e2389afe3
Amaan Shaikh <nerdofdot@gmail.com>
2021-11-24 20:43:57 +0530

    V1

---
+++ servo_hand/android/app/src/main/kotlin/com/dotdevelopingteam/servo_hand/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dotdevelopingteam.servo_hand
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ servo_hand/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: MainPage(),
+      backgroundColor: Colors.white,
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return 
+  }
+}
+
